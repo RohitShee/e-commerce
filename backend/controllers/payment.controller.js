467f4110c1b6f77b0538c09259561a85eea02791
RohitShee <rohitshee92@gmail.com>
2025-03-07 23:37:52 +0530

    add analytics routes

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -1,8 +1,62 @@
-
+import Coupon from "../models/coupon.model.js";
+import Order from "../models/order.model.js";
 export const getPaymentDetails= async(req,res) =>{
     try {
-        
-    } catch (error) {
-        
-    }
+		const { products, couponCode } = req.body;
+
+		if (!Array.isArray(products) || products.length === 0) {
+			return res.status(400).json({ error: "Invalid or empty products array" });
+		}
+
+		let totalAmount = 0;
+
+		const lineItems = products.map((product) => {
+			const amount = Math.round(product.price * 100); 
+			totalAmount += amount * product.quantity;
+
+			return {
+				price_data: {
+					currency: "usd",
+					product_data: {
+						name: product.name,
+						images: [product.image],
+					},
+					unit_amount: amount,
+				},
+				quantity: product.quantity || 1,
+			};
+		});
+
+		let coupon = null;
+		if (couponCode) {
+			coupon = await Coupon.findOne({ code: couponCode, userId: req.user._id, isActive: true });
+			if (coupon) {
+				totalAmount -= Math.round((totalAmount * coupon.discountPercentage) / 100);
+			}
+		}
+
+		
+		if (totalAmount >= 20000) {
+			await createNewCoupon(req.user._id);
+		}
+		res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
+	} catch (error) {
+		console.error("Error processing checkout:", error);
+		res.status(500).json({ message: "Error processing checkout", error: error.message });
+	}
+}
+
+async function createNewCoupon(userId) {
+	await Coupon.findOneAndDelete({ userId });
+
+	const newCoupon = new Coupon({
+		code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+		discountPercentage: 10,
+		expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+		userId: userId,
+	});
+
+	await newCoupon.save();
+
+	return newCoupon;
 }
